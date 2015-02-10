4fafdca8bbff8e03f13f0546bad7b8a0472c79e2
Mark Perry <maperry78@yahoo.com.au>
2015-02-11 02:26:25 +1000

    Simple call of java from frege

---
+++ src/main/java/com/github/mperry/frege/Bridge.java
@@ -0,0 +1,28 @@
+package com.github.mperry.frege;
+
+import fj.Unit;
+
+import static fj.Unit.unit;
+
+/**
+ * Created by MarkPerry on 10/02/2015.
+ */
+public class Bridge {
+
+
+    public static Unit fregeJava(Unit u) {
+        System.out.println("in fregeJava");
+        return unit();
+    }
+
+    public static Unit fregeJava2() {
+        System.out.println("in fregeJava2");
+        return unit();
+    }
+
+    public static Unit javaFrege(Unit u) {
+        System.out.println("in javaFrege");
+        return unit();
+    }
+
+}
