0050a42f8453a58dd949ce6bb1a2218c886483d3
Mark Perry <mark.perry@revelian.com>
2015-02-11 12:55:55 +1000

    Added second module

---
+++ mod2/src/main/java/com/github/mperry/frege/Facade.java
@@ -0,0 +1,19 @@
+package com.github.mperry.frege;
+
+import fj.Unit;
+
+import static fj.Unit.unit;
+
+/**
+ * Created by MarkPerry on 10/02/2015.
+ */
+public class Facade {
+
+    // call Frege code
+
+    public static void main(String [] args) {
+
+        System.out.println("Facade.main");
+    }
+
+}
